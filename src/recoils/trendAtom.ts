6de2038bf7ca05b4a7a2ab8a299c81e7d11a3874
Yangseungchan <uvzone@naver.com>
2023-04-20 18:47:23 +0900

    chore: 린트 반영

--- src/recoils/trendAtom.ts
+++ src/recoils/trendAtom.ts
@@ -1,7 +1,8 @@
 import { atom } from 'recoil';
 
-import { type Trend } from '@/types/data';
 import { v4 } from 'uuid';
+
+import { type Trend } from '@/types/data';
 
 const trendAtom = atom<Trend>({
   key: `trendAtom/${v4()}`,
