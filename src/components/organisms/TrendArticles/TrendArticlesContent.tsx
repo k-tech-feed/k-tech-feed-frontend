7895c2f79e0fc92d8ae1e4d894a42cfc6d8151f5
Yangseungchan <uvzone@naver.com>
2023-04-20 21:26:04 +0900

    chore: 인기 아티클 길이 제한

--- src/components/organisms/TrendArticles/TrendArticlesContent.tsx
+++ src/components/organisms/TrendArticles/TrendArticlesContent.tsx
@@ -14,17 +14,15 @@ const TrendArticlesContent = ({ isMobile }: Props) => {
   const { data: trends } = useTrendingArticlesQuery({ type: trendType });
   return (
     <div>
-      {trends
-        ?.slice(0, isMobile ? 1 : trends.length)
-        .map(({ id, title, author, timestamp }, idx) => (
-          <TrendArticleItem
-            key={idx}
-            href={`${process.env.NEXT_PUBLIC_API_URL as string}/articles/${id}`}
-            title={title}
-            author={author}
-            timestamp={timestamp}
-          />
-        ))}
+      {trends?.slice(0, isMobile ? 1 : 3).map(({ id, title, author, timestamp }, idx) => (
+        <TrendArticleItem
+          key={idx}
+          href={`${process.env.NEXT_PUBLIC_API_URL as string}/articles/${id}`}
+          title={title}
+          author={author}
+          timestamp={timestamp}
+        />
+      ))}
     </div>
   );
 };
